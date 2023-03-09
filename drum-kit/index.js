effd61cc05d8223dfe30076366c97d1bb76f6829
Faiz Mokhtar <mfmokhtar@gmail.com>
2023-03-09 22:03:35 +0800

    cleanup

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -2,13 +2,8 @@
 
 document.addEventListener("keydown", function(event) {
     const key = event.keyCode;
-
     updateKeyboardElementState(key, true);
-
-    const audioEl = document.querySelector(`audio[data-key="${key}"]`);
-    if (!audioEl) return;
-    audioEl.currentTime = 0;
-    audioEl.play();
+    playSound(key);
 })
 
 document.addEventListener("keyup", function(event) {
@@ -20,4 +15,11 @@ function updateKeyboardElementState(key, isPressed) {
     const divEl = document.querySelector(`div[data-key="${key}"]`);
     if (!divEl) return;
     isPressed ? divEl.classList.add("playing") : divEl.classList.remove("playing");
+}
+
+function playSound(key) {
+    const audioEl = document.querySelector(`audio[data-key="${key}"]`);
+    if (!audioEl) return;
+    audioEl.currentTime = 0;
+    audioEl.play();
 }
