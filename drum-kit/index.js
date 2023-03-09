9e460813a4967e1b9b2280db0035e74c3b7d642e
Faiz Mokhtar <mfmokhtar@gmail.com>
2023-03-09 21:59:05 +0800

    add working drumkit demo

---
+++ drum-kit/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+document.addEventListener("keydown", function(event) {
+    const key = event.keyCode;
+
+    updateKeyboardElementState(key, true);
+
+    const audioEl = document.querySelector(`audio[data-key="${key}"]`);
+    if (!audioEl) return;
+    audioEl.currentTime = 0;
+    audioEl.play();
+})
+
+document.addEventListener("keyup", function(event) {
+    const key = event.keyCode;
+    updateKeyboardElementState(key, false);
+});
+
+function updateKeyboardElementState(key, isPressed) {
+    const divEl = document.querySelector(`div[data-key="${key}"]`);
+    if (!divEl) return;
+    isPressed ? divEl.classList.add("playing") : divEl.classList.remove("playing");
+}
